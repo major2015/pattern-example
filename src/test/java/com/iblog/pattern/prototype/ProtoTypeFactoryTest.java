2992118120de7caf3bc8fcee06b985f66d7da163
xushaoxiao <xushaoxiao@flashhold.com>
2019-04-24 19:19:35 +0800

    add unittest

---
+++ src/test/java/com/iblog/pattern/prototype/ProtoTypeFactoryTest.java
@@ -0,0 +1,18 @@
+package com.iblog.pattern.prototype;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProtoTypeFactoryTest {
+
+    @Test
+    public void getInstance() throws CloneNotSupportedException {
+        Movie instance = (Movie) ProtoTypeFactory.getInstance(ProtoTypeFactory.ModelType.MOVIE);
+        Movie instance2 = (Movie) ProtoTypeFactory.getInstance(ProtoTypeFactory.ModelType.MOVIE);
+        instance.setName("1");
+        instance2.setName("2");
+        assertEquals("1", instance.getName());
+        assertEquals("2", instance2.getName());
+    }
+}

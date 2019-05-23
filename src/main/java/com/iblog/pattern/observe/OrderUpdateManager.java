d353075fab36e12c8f9b09dddadc02261a3b0480
xushaoxiao <xushaoxiao@flashhold.com>
2019-05-23 14:10:02 +0800

    fill order status update func

--- src/main/java/com/iblog/pattern/observe/OrderUpdateManager.java
+++ src/main/java/com/iblog/pattern/observe/OrderUpdateManager.java
@@ -1,11 +1,41 @@
 package com.iblog.pattern.observe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderUpdateManager {
+    private final List<Observer> observers = new ArrayList<>();
     private final Notifier notifier;
 
-    public OrderUpdateManager(Notifier notifier) {
-        this.notifier = notifier;
+    public OrderUpdateManager() {
+        this.notifier = new Notifier(observers);
     }
 
+    public void register(Observer observer) {
+        synchronized (observers) {
+            observers.add(observer);
+        }
+    }
 
+    public void unregister(Observer observer) {
+        synchronized (observers) {
+            observers.remove(observer);
+        }
+    }
+
+    public void awaitpaidOrder(Order order) {
+        notifier.notify(order, Order.Status.AWAIT_PAYMENT, "paid order");
+    }
+
+    public void receivedOrder(Order order) {
+        notifier.notify(order, Order.Status.RECEIVED, "await payment");
+    }
+
+    public void paidOrder(Order order) {
+        notifier.notify(order, Order.Status.PAYMENT_RECEIVED, "will process order");
+    }
+
+    public void delivered(Order order) {
+        notifier.notify(order, Order.Status.SHIPPED, "delivered");
+    }
 }
